3a70f8dbd5ad4a23457c58857fe819f32b3f0a21
Momand Wiar <65156571+MomandWiar@users.noreply.github.com>
2024-02-08 12:00:54 +0100

    Add service class

--- src/main/java/com/example/postcodecli/PostCodeClient.java
+++ src/main/java/com/example/postcodecli/PostCodeClient.java
@@ -1,12 +1,21 @@
 package com.example.postcodecli;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 
 import java.util.Scanner;
 
 @Component
 public class PostCodeClient implements CommandLineRunner {
+
+    PostCodeService postCodeService;
+
+    public PostCodeClient (PostCodeService postCodeService) {
+        this.postCodeService = postCodeService;
+    }
 
     @Override
     public void run(String... args) {
@@ -19,6 +28,13 @@ public class PostCodeClient implements CommandLineRunner {
 
             if ("exit".equalsIgnoreCase(postcode) || "exit".equalsIgnoreCase(houseNumber)) {
                 System.exit(0);
+            }
+
+            try {
+                ResponseEntity<String> response = postCodeService.getAddress(postcode, houseNumber);
+                System.out.println(response.getBody());
+            } catch (HttpClientErrorException | HttpServerErrorException e) {
+                System.out.println("Something went wrong, " + e.getStatusCode());
             }
         }
     }
--- src/main/java/com/example/postcodecli/PostCodeService.java
+++ src/main/java/com/example/postcodecli/PostCodeService.java
@@ -1,7 +1,37 @@
 package com.example.postcodecli;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Service
 public class PostCodeService {
+
+    @Value("${application.baseURL}")
+    private String baseURL;
+
+    @Value("${application.accessToken}")
+    private String accessToken;
+
+    public ResponseEntity<String> getAddress(String postcode, String houseNumber) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        String URI = UriComponentsBuilder.fromUriString(baseURL)
+                .queryParam("postcode", postcode)
+                .queryParam("number", houseNumber)
+                .build().toUriString();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + accessToken);
+        headers.set("Content-Type", "application/json");
+
+        HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
+
+        return restTemplate.exchange(URI, HttpMethod.GET, entity, String.class);
+    }
 }
